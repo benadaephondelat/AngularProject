db99e2227f9d24b36de7558fbdb5b893e7efdb06
benadaephondelat <georgi_iliev@yahoo.com>
2015-05-19 17:07:12 +0300

    profile and cover picture

--- app/js/controllers/editProfileController.js
+++ app/js/controllers/editProfileController.js
@@ -26,6 +26,33 @@ FuckBook.controller("EditProfileController", function ($scope, editProfileServic
             }, function (error) {
                 console.log(error);
             })
-    }
+    };
 
+    $scope.uploadProfilePicture = function () {
+        selectFile("profilePicture", "profilePicturePreview");
+    };
+
+    $scope.uploadCoverPicture = function () {
+        selectFile("coverPicture", "coverPicturePreview");
+    };
+
+    function selectFile(inputSelector, picturePreview){
+        $('body').on('change', "#" + inputSelector, function() {
+            var fileInput = document.getElementById(inputSelector);
+            var file = fileInput.files[0];
+            var reader = new FileReader();
+            reader.onload = function () {
+                var targetSource = document.getElementById(picturePreview);
+                targetSource.src = reader.result;
+                if(inputSelector == "profilePicture"){
+                    $scope.editData.profileImageData = targetSource.src;
+                }
+                else{
+                    $scope.editData.coverImageData = targetSource.src;
+                }
+            };
+
+            reader.readAsDataURL(file);
+        });
+    }
 });
